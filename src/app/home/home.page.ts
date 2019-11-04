449db322954ac37eb088e07c28a78533519056e5
Luis Fernando Espindola Silva <lespindola@ucol.mx>
2019-11-04 15:58:34 -0600

    notification wf

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,8 +7,8 @@ import { ActionSheetController, Platform, AlertController } from '@ionic/angular
 import { ActivatedRoute } from '@angular/router';
 import { ApiService } from './../services/api.service'
 import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
-
-
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import equal from 'deep-equal';
 
 @Component({
   selector: 'app-home',
@@ -16,118 +16,156 @@ import { LocalNotifications } from '@ionic-native/local-notifications/ngx';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
-  private url: string ="https://semaforosinteligentes.000webhostapp.com/sensores/index.php";
+  private url: string = "https://semaforosinteligentes.000webhostapp.com/sensores/index.php";
   sensors: Observable<any>;
-  items:any;
-  sensor:any;
+  items: any;
+  sensor: any;
   isFavorite = false;
   idControl = null;
-  data2:any;
-  constructor(private alertCtrl: AlertController, private plt: Platform, private favoriteService: FavoriteService ,private api: ApiService, 
-    private activatedRoute: ActivatedRoute, private router: Router, private http: HttpClient, 
-    public actionSheetController: ActionSheetController, private localNotifications: LocalNotifications ) {}
-  
+  data2: any;
+
+  constructor(private statusBar: StatusBar, private plt: Platform, private favoriteService: FavoriteService, private api: ApiService,
+    private activatedRoute: ActivatedRoute, private router: Router, private http: HttpClient,
+    public actionSheetController: ActionSheetController, private localNotifications: LocalNotifications) { }
+
   ngOnInit() {
-    this.idControl = this.activatedRoute.snapshot.paramMap.get('id');
-    this.api.getSensor(this.idControl).subscribe(res => {
-      this.sensor = res;
-    });
+    this.statusBar.overlaysWebView(true);
+
+    this.statusBar.backgroundColorByHexString('#ffffff');
     
- 
-    this.favoriteService.isFavorite(this.idControl).then(isFav => {
-      this.isFavorite = isFav;
+    this.statusBar.show();
+    
+    this.sensors = this.http.get(this.url);
+    this.sensors.subscribe(data => {
+      this.items = data;
     });
 
-    this.sensors = this.http.get(this.url); 
-    this.sensors.subscribe(data => {
-        this.items = data;
+    const timer = setInterval(async () => {
+      await new Promise((resolve, reject) => {
+        this.http.get(this.url).subscribe(newData => {
+          this.data2 = newData || [];
+          resolve(newData);
+        });
       });
 
-    var timer = setInterval(()=> {
-      const newdata = this.http.get(this.url); 
-    newdata.subscribe(nwdata => {
-        this.data2 = nwdata;
-      });
-      if (this.items !== this.data2){
-        this.simpleNotif(this.items);
-      }else if (this.items == this.data2){
+      const isEqual = equal(this.items, this.data2)
 
+      if (isEqual) return
+
+      const favoriteList = (await this.favoriteService.getAllFavoriteSensors()) || []
+
+      function extractProps(obj, props) {
+        return props.map(prop => obj[prop])
       }
-  },4000); 
+
+      if (favoriteList.length > 0) {
+        console.log(favoriteList)
+        const favorites = this.data2.filter(({ idcontrol }) => favoriteList.includes(idcontrol));
+        console.log(favorites)
+  
+        const props = ['sensor1', 'sensor2', 'sensor3']
+        const favoritesChanges = favorites.filter(favorite => {
+          const idControl = favorite['idcontrol'];
+          const sensor = this.items.find(item => item.idcontrol === idControl);
+          return !equal(extractProps(sensor, props), extractProps(favorite, props));
+        });
+        favoritesChanges.forEach(item => this.simpleNotif(item));
+      }
+
+      this.items = this.data2
+    }, 4000);
   }
 
   simpleNotif(res) {
-    this.localNotifications.schedule({
-      id: res.idControl,
-      text: res.nombre + 'en proceso de inundación',
-      data: { mydata: 'buenas' },
-    });
+    if(res.sensor1 + '' + res.sensor2 + '' +res.sensor3 == '111'){
+      this.localNotifications.schedule({
+        id: res.idcontrol,
+        text: res.nombre + ' Vialidad Inundada',
+        smallIcon: 'https://www.stickpng.com/assets/images/58afdad6829958a978a4a693.png',
+        icon: 'https://previews.123rf.com/images/nikdoorg/nikdoorg1511/nikdoorg151100027/47920820-s%C3%ADmbolo-del-sem%C3%A1foro.jpg',
+        data: { mydata: 'buenas' }
+      });
+    } else if (res.sensor1 + res.sensor2 + res.sensor3 == '110'){
+      this.localNotifications.schedule({
+        id: res.idcontrol,
+        text: res.nombre + ' Vialidad en proceso de Inundada',
+        icon: 'https://previews.123rf.com/images/nikdoorg/nikdoorg1511/nikdoorg151100027/47920820-s%C3%ADmbolo-del-sem%C3%A1foro.jpg',
+        data: { mydata: 'buenas' }
+      });
+    }else{
+      this.localNotifications.schedule({
+        id: res.idcontrol,
+        text: res.nombre + ' Vialidad Libre',
+        icon: 'https://previews.123rf.com/images/nikdoorg/nikdoorg1511/nikdoorg151100027/47920820-s%C3%ADmbolo-del-sem%C3%A1foro.jpg',
+        data: { mydata: 'buenas' }
+      });
+    }
   }
 
-  favoriteSensor() {
-    this.favoriteService.favoriteSensor(this.idControl).then(() => {
-      this.isFavorite = true;
-    });
+  favoriteSensor({ idcontrol }) {
+    this.favoriteService.favoriteSensor(idcontrol);
   }
- 
-  unfavoriteSensor() {
-    this.favoriteService.unfavoriteFilm(this.idControl).then(() => {
-      this.isFavorite = false;
-    });
+
+  unfavoriteSensor({ idcontrol }) {
+    this.favoriteService.unfavoriteFilm(idcontrol);
   }
+
   doRefresh(refresher) {
     console.log('Begin async operation');
-      this.sensors.subscribe(data => {
+    this.sensors.subscribe(data => {
       this.sensors = this.http.get(this.url);
       this.items = data;
       refresher.target.complete();
-    }); 
-  }
-  async presentActionSheet(data){
-    let flag;
-if (this.isFavorite){
-  const actionSheet = await this.actionSheetController.create({
-    header:  data.nombre,
-    subHeader: data.municipio,
-    buttons: [{
-      text: 'Desactivar Notificaciones',
-      icon: 'heart',
-      handler: () => {
-          this.unfavoriteSensor();
-          console.log("works!");
-      }
-    }, {
-      text: 'Cancel',
-      icon: 'close',
-      role: 'cancel',
-      handler: () => {
-      }
-    }]
-  });
-  await actionSheet.present();
-}else{
-  const actionSheet = await this.actionSheetController.create({
-    header:  data.nombre,
-    subHeader: data.municipio,
-    buttons: [{
-      text: 'Activar Notificaciones',
-      icon: 'heart',
-      handler: () => {
-          this.favoriteSensor();
-          console.log("asiesworks!");
-      }
-    }, {
-      text: 'Cancel',
-      icon: 'close',
-      role: 'cancel',
-      handler: () => {
-        console.log("works!no");
-      }
-    }]
-  });
-  await actionSheet.present();
-}
+    });
   }
 
+  async presentActionSheet(data) {
+    // let flag;
+
+    const idControl = data.idcontrol;
+    const isFavorite = await this.favoriteService.isFavorite(idControl);
+
+    if (isFavorite) {
+      const actionSheet = await this.actionSheetController.create({
+        header: data.nombre,
+        subHeader: data.municipio,
+        buttons: [{
+          text: 'Desactivar Notificaciones',
+          icon: 'heart',
+          handler: () => {
+            this.unfavoriteSensor(data);
+            console.log("works!");
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+          }
+        }]
+      });
+      await actionSheet.present();
+    } else {
+      const actionSheet = await this.actionSheetController.create({
+        header: data.nombre,
+        subHeader: data.municipio,
+        buttons: [{
+          text: 'Activar Notificaciones',
+          icon: 'heart',
+          handler: () => {
+            this.favoriteSensor(data);
+            console.log("asiesworks!");
+          }
+        }, {
+          text: 'Cancel',
+          icon: 'close',
+          role: 'cancel',
+          handler: () => {
+            console.log("works!no");
+          }
+        }]
+      });
+      await actionSheet.present();
+    }
+  }
 }
